d151769e5ddfc2fe26976f7e80c5c3faab31d703
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-10 23:25:27 +0300

    Add an example of XMLValidator use

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -3,6 +3,7 @@ package ua.knu.csc;
 public class Main {
 
     public static void main(String[] args) {
+        XMLValidator.validate("data/departments.xml", "data/departments.xsd");
         DOMBuilder domBuilder = new DOMBuilder();
         System.out.println(domBuilder.build("data/departments.xml"));
     }
