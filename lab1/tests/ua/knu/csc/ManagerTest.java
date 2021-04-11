98ceced118e8ad07c9de118c471a6c5dfd5a525e
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-11 18:02:09 +0300

    Create an empty class to test a Manager class

---
+++ lab1/tests/ua/knu/csc/ManagerTest.java
@@ -0,0 +1,37 @@
+package ua.knu.csc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addDepartment() {
+    }
+
+    @Test
+    void getDepartment() {
+    }
+
+    @Test
+    void deleteDepartment() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void getEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
