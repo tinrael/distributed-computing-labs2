5c8214ba13dd99d311498bda98effcc174d9ccdd
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-11 23:52:50 +0300

    Add an example of use

--- lab1/src/ua/knu/csc/Main.java
+++ lab1/src/ua/knu/csc/Main.java
@@ -1,10 +1,35 @@
 package ua.knu.csc;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
         XMLValidator.validate("data/departments.xml", "data/departments.xsd");
-        DOMBuilder domBuilder = new DOMBuilder();
-        System.out.println(domBuilder.build("data/departments.xml"));
+
+        Manager manager = new Manager();
+
+        manager.loadFromFile("data/departments.xml");
+
+        System.out.println("--- --- Before --- ---");
+        manager.print();
+        System.out.println("--- --- ---- --- ---");
+
+        manager.addDepartment("d-2", "marketing");
+        manager.addEmployee("e-3", "Olivia", "Brown", BigInteger.valueOf(1200), "d-2");
+
+        manager.getEmployee("e-1").setSalary(BigInteger.valueOf(700));
+
+        manager.deleteEmployee("e-2");
+
+        manager.deleteDepartment("d-0");
+
+        manager.saveToFile("data/departments-new.xml");
+
+        manager.loadFromFile("data/departments-new.xml");
+
+        System.out.println("--- --- After --- ---");
+        manager.print();
+        System.out.println("--- --- ---- --- ---");
     }
 }
