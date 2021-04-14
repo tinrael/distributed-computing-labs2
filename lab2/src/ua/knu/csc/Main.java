d5396b04c6df062bc842f2b5c8aab93f235558d9
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-14 23:00:23 +0300

    Connecting to the database

--- lab2/src/ua/knu/csc/Main.java
+++ lab2/src/ua/knu/csc/Main.java
@@ -1,8 +1,19 @@
 package ua.knu.csc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String url = "jdbc:postgresql://localhost/departments";
+        String user = "postgres";
+        String password = "postgres";
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
