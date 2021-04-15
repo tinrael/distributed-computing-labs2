ff137ea0743836e528de2d8ad9df9795ee60ff58
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-15 14:45:17 +0300

    Update the method 'addDepartment' and add the method 'deleteDepartment'

--- lab2/src/ua/knu/csc/core/Manager.java
+++ lab2/src/ua/knu/csc/core/Manager.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Manager {
-    private Connection connection;
-    private Statement statement;
+    private final Connection connection;
+    private final Statement statement;
 
     public Manager(String url, String user, String password) throws SQLException {
         connection = DriverManager.getConnection(url, user, password);
@@ -18,9 +18,39 @@ public class Manager {
         connection.close();
     }
 
-    public void addDepartment(String id, String name) throws SQLException {
+    public boolean addDepartment(String id, String name) {
         String sql = "INSERT INTO department VALUES" +
                 "('" + id + "', '" + name + "');";
-        statement.executeUpdate(sql);
+
+        try {
+            statement.executeUpdate(sql);
+
+            System.out.println("[SUCCESS]: The " + name + " department successfully added.");
+            return true;
+        } catch (SQLException throwable) {
+            System.out.println("[FAIL]: Unable to add the " + name + " department.");
+            throwable.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteDepartment(String id) {
+        String sql = "DELETE FROM department WHERE department_id = '" + id + "';";
+
+        try {
+            int rowCount = statement.executeUpdate(sql);
+
+            if (rowCount > 0) {
+                System.out.println("[SUCCESS]: The department with the identifier '" + id + "' successfully deleted.");
+                return true;
+            } else {
+                System.out.println("[FAIL]: The department with the identifier '" + id + "' not found.");
+                return false;
+            }
+        } catch (SQLException throwable) {
+            System.out.println("[FAIL]: Unable to delete the department with the identifier '" + id + "'.");
+            throwable.printStackTrace();
+            return false;
+        }
     }
 }
