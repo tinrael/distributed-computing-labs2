64aaf3cbd0c2fda46ab9f454854a935604849602
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-16 21:57:20 +0300

    Update the Main class to create and show GUI

--- lab2/src/ua/knu/csc/Main.java
+++ lab2/src/ua/knu/csc/Main.java
@@ -1,8 +1,16 @@
 package ua.knu.csc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import ua.knu.csc.core.Manager;
+import ua.knu.csc.ui.EmployeePanel;
+
 import java.sql.SQLException;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class Main {
 
@@ -10,10 +18,31 @@ public class Main {
         String url = "jdbc:postgresql://localhost/departments";
         String user = "postgres";
         String password = "postgres";
+
         try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            Manager manager = new Manager(url, user, password);
+
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGUI(manager);
+                }
+            });
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
         }
+    }
+
+    private static void createAndShowGUI(Manager manager) {
+        JFrame mainWindow = new JFrame("lab2");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainWindow.add(new EmployeePanel(manager));
+
+        mainWindow.pack();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        mainWindow.setLocation(screenSize.width / 2 - mainWindow.getSize().width / 2, screenSize.height / 2 - mainWindow.getSize().height / 2);
+
+        mainWindow.setVisible(true);
     }
 }
