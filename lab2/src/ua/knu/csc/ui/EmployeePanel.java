fc79a2092bc19616ee171e18fd9985452d2b8013
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-04-16 21:39:46 +0300

    Create the EmployeePanel with methods 'refreshEmployeeIdsComboBox' and 'addEmployee'

---
+++ lab2/src/ua/knu/csc/ui/EmployeePanel.java
@@ -0,0 +1,93 @@
+package ua.knu.csc.ui;
+
+import ua.knu.csc.core.Manager;
+
+import java.util.List;
+
+import java.math.BigInteger;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.GridLayout;
+
+public class EmployeePanel extends JPanel {
+    private final Manager manager;
+
+    private final JComboBox<String> employeeIdsComboBox = new JComboBox<>();
+
+    private final JTextField employeeIdTextField = new JTextField();
+    private final JTextField forenameTextField = new JTextField();
+    private final JTextField surnameTextField = new JTextField();
+    private final JTextField salaryTextField = new JTextField();
+    private final JTextField departmentIdTextField = new JTextField();
+
+    private final JButton buttonAdd = new JButton("Add");
+    private final JButton buttonUpdate = new JButton("Update");
+    private final JButton buttonDelete = new JButton("Delete");
+
+    public EmployeePanel(Manager manager) {
+        this.manager = manager;
+
+        setLayout(new GridLayout(8, 2));
+
+        add(new JLabel("choose employee id"));
+        add(employeeIdsComboBox);
+
+        add(new JLabel("employee id"));
+        add(employeeIdTextField);
+
+        add(new JLabel("forename"));
+        add(forenameTextField);
+
+        add(new JLabel("surname"));
+        add(surnameTextField);
+
+        add(new JLabel("salary"));
+        add(salaryTextField);
+
+        add(new JLabel("department id"));
+        add(departmentIdTextField);
+
+        add(buttonAdd);
+        add(buttonDelete);
+
+        add(buttonUpdate);
+
+        buttonAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addEmployee();
+            }
+        });
+
+        refreshEmployeeIdsComboBox();
+    }
+
+    private void refreshEmployeeIdsComboBox() {
+        employeeIdsComboBox.removeAllItems();
+
+        List<String> allEmployeeIds = manager.getAllEmployeeIds();
+
+        for (String employeeId : allEmployeeIds) {
+            employeeIdsComboBox.addItem(employeeId);
+        }
+    }
+
+    private void addEmployee() {
+        boolean isSuccessfullyAdded = manager.addEmployee(employeeIdTextField.getText(), forenameTextField.getText(),
+                surnameTextField.getText(), new BigInteger(salaryTextField.getText()),
+                departmentIdTextField.getText());
+
+        if (isSuccessfullyAdded) {
+            refreshEmployeeIdsComboBox();
+            employeeIdsComboBox.setSelectedItem(employeeIdTextField.getText());
+        }
+    }
+}
